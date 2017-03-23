3aecb47d233472c307785a0ac19122748ce54b2c
Martina Vitovska <409920@mail.muni.cz>
2017-03-23 13:51:37 +0100

    Add new benchmark
    Signed-off-by: Martina Vitovska <409920@mail.muni.cz>

---
+++ memory-stack/stack02-false.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+int areNatural(int *numbers){
+	int i = 0;
+	while(i < 10) {
+		if(numbers[i] <= 0){
+			return 0;
+		}
+		i++;
+	}
+	return 1;
+}
+
+int main() {
+	char numbers[] = {0,1,2,3,4,5,6,7,8,9};	
+	int result = areNatural((int*) numbers);
+	return 0;
+}
