9bfefc9d8c2fe44ee423ee71c77dfc1cc8fe38a2
Martina Vitovska <409920@mail.muni.cz>
2017-03-24 09:21:56 +0100

    Add new correct benchmark
    Signed-off-by: Martina Vitovska <409920@mail.muni.cz>

---
+++ memory-stack/stack03-true.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// function returns array of numbers
+int * getNumbers() {
+
+   static int array[10]; // array is static, which is correct
+  
+   for (int i = 0; i < 10; ++i) {
+      array[i] = i;
+   }
+
+   return array;
+}
+
+int main (void) {
+
+   int *numbers = getNumbers();
+	
+   for (int i = 0; i < 10; i++ ) {
+      printf( "%d\n", *(numbers + i));
+   }
+
+   return 0;
+}
