e3ce4e85672790090c7bf03efa144a94e6ab5ac0
Martina Vitovska <409920@mail.muni.cz>
2017-08-29 13:06:14 +0200

    Add benchmarks for memssafety
    Returning pointer to a memory allocated on heap.

---
+++ memory-safety/valid-deref01-true.c
@@ -0,0 +1,24 @@
+#include<stdlib.h>
+
+int* getArray(size){
+    int* array = malloc(size * sizeof(int));
+    
+    for (int i = 0; i < size; i++) {
+        array[i] = 0;
+    }
+
+    return array;
+}
+
+int main(void) {
+    int size = 10;
+    int* array = getArray(size);
+
+    for (int i = 0; i < size; i++) {
+        array[i] = 1;
+    }
+
+    free(array);
+
+    return 0;
+}
